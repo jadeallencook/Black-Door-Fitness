10df717f703e3b471d0e5f571583028061522061
Jade Allen Cook <jadeallencook@gmail.com>
2016-04-28 09:28:10 -0600

    ⚗ Reconfigured code

--- php/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-    <center>
-    
-        <small>Black Door Fitness Club Copyright 2015 - Coming To South Jordan, UT</small>
-    
-        <br /><br /><br />
-    </center>
-        
-</div>
